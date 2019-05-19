35bed2f8e10de43b1b72b32ec9fc8765a559e40c
Ryan Gonzales <helloryantg@gmail.com>
2019-05-18 17:09:41 -0700

    First commit

---
+++ src/com/rgonz/Bank.java
@@ -0,0 +1,33 @@
+package com.rgonz;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+
+        return false;
+    }
+
+
+}
